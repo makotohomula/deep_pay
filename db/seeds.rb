af30d487e5e0d01f09f75f9ab5b5c239b375c081
邹逸(实习) <zouyi@megvii.com>
2019-09-17 00:34:21 +0800

    add Products Controller && add CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+Product.create!(
+    title: 'HHKB Professional BT蓝牙版',
+    description: 'HHKB Professional BT 白色有刻版 静电容键盘 蓝牙键盘 编程专用布局 60键',
+    image_url: 'hhkb.jpg',
+    price: 2188.00
+)
